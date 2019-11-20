4d796006a5c17a37c871a00564a25c9bdf5a00ef
ValeriCR51017019 <48231242+ValeriCR51017019@users.noreply.github.com>
2019-11-20 21:57:34 +0700

    Add files via upload

---
+++ Operasi.rs
@@ -0,0 +1,12 @@
+Operasi.Operasi$1
+Operasi.Operasi$2
+GUI.tambahkurangkalibagi
+Operasi.Operasi$3
+Operasi.Operasi
+Operasi.Operasi$4
+GUI.tambahkurangkalibagi$2
+Operasi.Operasi$5
+GUI.tambahkurangkalibagi$3
+GUI.tambahkurangkalibagi$4
+GUI.tambahkurangkalibagi$5
+GUI.tambahkurangkalibagi$1
